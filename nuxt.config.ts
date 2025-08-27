8695a52279fef091a696324831985813cefc5a27
Ar Kar Moe <moearkar963@gmail.com>
2025-08-27 23:04:16 +0630

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  alias: {
+    "@": './',
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  typescript: {
+    typeCheck: true
+  },
+
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
